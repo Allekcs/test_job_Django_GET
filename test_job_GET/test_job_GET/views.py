e978532b68d8a9e6d6cd7daedf34d3e47b1de9a9
Allekcs <allexpestovskii@gmail.com>
2023-05-24 02:14:01 +0300

    all_code

---
+++ test_job_GET/test_job_GET/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    name = request.GET.get("name", "Recruto")
+    message = request.GET.get("message", "Давай дружить")
+    return HttpResponse(f"<h1>Hello {name}! {message}!</h1>")
